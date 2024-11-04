b6b865b32ff7d7d482dda89b780dc91bb497b3c8
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-04 15:37:04 +0000

    Created Plant and User models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.pre('save', (next) => {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
