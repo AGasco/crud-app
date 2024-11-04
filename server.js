54f12da664e23ae5fcc31825d02ef561fb922d35
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-04 16:59:03 +0000

    Added fallback responses to unknown or error endpoints

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ const usersRouter = require('./routes/users');
 app.use('/api/plants', plantsRouter);
 app.use('/api/users', usersRouter);
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Endpoint not found.' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
